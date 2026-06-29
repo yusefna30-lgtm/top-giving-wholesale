214b02c7fabeb2ed59bf179468145d3967c64155
yusefna30-lgtm <yusefna30@gmail.com>
2026-06-29 17:23:27 +0300

    Add function to update element quantity

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-
+function c(id, value) {
+    let element = document.getElementById(id);
+    let currentQty = parseInt(element.innerText) || 0;
+    let newQty = currentQty + value;
+    
+    if (newQty < 0) {
+        newQty = 0;
+    }
+    
+    element.innerText = newQty;
+}
