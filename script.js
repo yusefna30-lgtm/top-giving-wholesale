258658b33ecacf4a4c2e57349eea444d1311745d
yusefna30-lgtm <yusefna30@gmail.com>
2026-07-01 12:34:16 +0300

    Update script.js

--- script.js
+++ script.js
@@ -2,10 +2,10 @@ function c(id, value) {
     let element = document.getElementById(id);
     let currentQty = parseInt(element.innerText) || 0;
     let newQty = currentQty + value;
-
+    
     if (newQty < 0) {
         newQty = 0;
     }
-
+    
     element.innerText = newQty;
 }
